7e6dd05973883d4bcbb9c0975cf7454dc7675e61
Mystery-Coder <srikar0811@gmail.com>
2023-07-29 13:12:21 +0530

    React App

--- quizzo-react-app/src/App.js
+++ quizzo-react-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const inputRef = useRef();
+	return (
+		<div>
+			<h1>Quizzo - Play and Make Your Own Quizzes</h1>
+			<br />
+			<input placeholder="Enter Quiz Name" ref={inputRef} /> <br />
+			<button onClick={() => console.log(inputRef.current.value)}>
+				Play Quiz
+			</button>
+			<button onClick={() => console.log(inputRef.current.value)}>
+				Create New Quiz
+			</button>
+		</div>
+	);
 }
 
 export default App;
